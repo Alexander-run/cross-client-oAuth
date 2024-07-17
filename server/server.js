baadc08c074067b1fc104f932100d5c84ba73f27
Helios <helios@192.168.0.102>
2024-07-17 09:04:24 +0800

    update server and cx connect to localhost

--- server/server.js
+++ server/server.js
@@ -1,29 +1,40 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3050 });
-
-wss.on('connection', (ws) => {
+const wsClient = new WebSocket.Server({ port: 3050 });
+const wsInterceptor = new WebSocket.Server({port:3051})
+let connectionCount = 0
+wsClient.on('connection', (ws) => {
   console.log('Client connected');
-
+  connectionCount++
   ws.on('message', (message) => {
     console.log(`Received: ${message}`);
     // 广播消息给所有连接的客户端
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+    wsInterceptor.clients.forEach((client) => {
+      console.log('sent once')
+      client.send(message);
     });
   });
-  // ping pong to maintain connection
-  const interval = setInterval(() => {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.ping()
-    }
-  })
-
+  console.log('当前连接数', connectionCount)
   ws.on('close', () => {
     console.log('Client disconnected');
-    if (interval) clearInterval(interval)
+    connectionCount--
+  });
+});
+wsInterceptor.on('connection', (ws) => {
+  console.log('Client connected');
+  connectionCount++
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    // 广播消息给所有连接的客户端
+    wsClient.clients.forEach((client) => {
+      console.log('sent once')
+      client.send(message);
+    });
+  });
+  console.log('当前连接数', connectionCount)
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    connectionCount--
   });
 });
 
